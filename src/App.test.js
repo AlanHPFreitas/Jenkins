d982489395c0b297d2ed5cd56fbbf32f600ecddd
AlanHPFreitas <131428856+AlanHPFreitas@users.noreply.github.com>
2025-04-01 09:42:57 -0600

    TestLocal

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen
+} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/This is my Jenkins App/i);
+  render( < App / > );
+  const linkElement = screen.getByText(/Alan Freitas/i);
   expect(linkElement).toBeInTheDocument();
 });
