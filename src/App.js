a8a6f0a1c8480a24686c59c6f5a7df3bd166bf15
AlanHPFreitas <131428856+AlanHPFreitas@users.noreply.github.com>
2025-04-03 17:56:26 -0600

    MistakeFixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is mY Jenkins App 
+          This is my Jenkins App 
         </p>
         <h1>
           Alan Freitas - Enterprise Computing
